1ed2036b9e77d7baffcaab955fde0c4a20388811
Alex Nielsen <nielsen6655@gmail.com>
2022-03-03 20:55:42 +0100

    add all my files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+//jsx fordi vi bruger emmit som shortcuts 
+
+
+// Functional component 
+
+import React from "react";
+
+const App = () => {
+    return (
+        <div>App</div>
+    )
+}
+
+export default App
