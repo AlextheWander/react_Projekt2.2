538bf2f1e8a382f1b734a44d037e62ff77a13412
Alex Nielsen <nielsen6655@gmail.com>
2022-03-05 16:05:26 +0100

    5/3/22

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './Header.css'
+import CTA from './CTA'
+import HeaderSocials from './HeaderSocials';
 
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <header>
+            <div className="container header__container">
+                <h5>Hello I'm</h5>
+                <h1>Alexander Nielsen</h1>
+                <h5 className='text-light'>Web Developer</h5>
+                <CTA />
+                <HeaderSocials />
+
+                <div className='me'>
+
+                </div>
+
+                <a href="#contact" className='scroll__down'>Sroll Down</a>
+            </div>
+        </header>
     )
 }
 
